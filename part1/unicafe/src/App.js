5940e6c1ab7ee5f3a176e5359905aa5c8a8f2072
Wilson <mkdirwilson@gmail.com>
2023-08-03 16:30:06 +0200

    complete Exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -35,13 +35,23 @@ const App = () =>
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
 
   // implement the button behaviours of all three feedbacks 
   const handleGoodClick = ()=> setGood(good + 1)
+
   const handleNeutralClick = ()=> setNeutral(neutral + 1)
+
   const handleBadClick = ()=> setBad(bad + 1)
 
+  // variable for all feedbacks
+  const total = good + neutral + bad;
+
+  // variable to compute the average
+  const average = (good - bad) / total || 0
+
+  // compute percentage of positive feedback
+  const positive_percentage = (good / total * 100) || 0
 
 
   return (
@@ -57,6 +67,9 @@ const App = () =>
     <FeedbackStats feedback="good" count={good}/>
     <FeedbackStats feedback="neutral" count={neutral}/>
     <FeedbackStats feedback="bad" count={bad}/>
+    <FeedbackStats feedback="all" count={total}/>
+    <FeedbackStats feedback="average" count={average}/>
+    <FeedbackStats feedback="positve" count={positive_percentage + "%"}/>
     </>
   )
 }
