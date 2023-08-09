719607f2bfdafb675a5970c4302c2fb3e2ebbdde
Wilson <mkdirwilson@gmail.com>
2023-08-09 02:43:09 +0200

    complete Exercise 2.8 - 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,15 +2,34 @@ import { useState } from "react";
 
 const App = ()=> {
 
-  const [persons, setPersons] = useState([{name: 'Arto Hellas'}])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
 
   const [newName, setNewName] = useState('')
+  
+  const [newNumber, setNewNumber] = useState('')
+
+  const [newFilter, setFilter] = useState('')
 
 
+  const handleFilterChange = (event)=> {
+    setFilter(event.target.value)
+  }
 
   const handlePersonChange = (event)=>{
     setNewName(event.target.value)
   }
+
+
+  const handleNumberChange = (event)=>{
+    setNewNumber(event.target.value)
+  }
+
+  
 
   const addPerson = (event)=>{
     event.preventDefault()
@@ -21,21 +40,35 @@ const App = ()=> {
     }
 
     else {
-      const newPerson = {name: newName}
+      const newPerson = {name: newName, number: newNumber}
       setPersons(persons.concat(newPerson))
     }
     
     setNewName('')
+    setNewNumber('')
   
   }
+
+
+  const numbersToShow = newFilter.length > 0 ? persons.filter((person)=>person.name.toLowerCase().startsWith(newFilter.toLowerCase())) : persons
 
 
   return (
     <>
       <h2>Phonebook</h2>
+      <p>
+        filter shown with: <input onChange={handleFilterChange} value={newFilter}></input>
+      </p>
+
+      <h3>Add a new contact</h3>
+
       <form onSubmit={addPerson}>
           <p>
-             name: <input onChange={handlePersonChange} value={newName}  placeholder="Enter name..."></input>
+             name: <input onChange={handlePersonChange} value={newName}  placeholder="Enter name..." ></input>
+          </p>
+
+          <p>
+            number: <input onChange={handleNumberChange} value={newNumber} placeholder="Enter number..." ></input>
           </p>
 
           <p>
@@ -44,8 +77,8 @@ const App = ()=> {
       </form>
 
       <h2>Numbers</h2>
-      {persons.map(person=>
-        <p key={person.name}>{person.name}</p>
+      {numbersToShow.map(person=>
+        <p key={person.name}>{person.name} {person.number}</p>
       )}
     </>
   )
